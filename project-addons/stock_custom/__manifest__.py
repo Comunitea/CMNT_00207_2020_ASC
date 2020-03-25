1ce25d2b7a928167a3b8ff46a51a78f6f73d286c
Kiko Sánchez <kiko@comunitea.com>
2020-03-26 00:43:36 +0100

    [DEV] stock_custom. Cusotmizaions over stock in asec

---
+++ project-addons/stock_custom/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2019 Comunitea - Kiko Sánchez
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock Custom A-SEC",
+    "summary": "Customization over warehouse",
+    "version": "12.0.1.0.0",
+    "author": "Comunitea",
+    "category": "Inventory",
+    "depends": ["stock"],
+    "data": [
+        "views/stock_location.xml",
+    ],
+    "installable": True,
+    "license": "AGPL-3",
+}
