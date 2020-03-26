a1a6f5986de62ed8ca2b12c07ed52088ed6a6d61
JesusVMayor <jesus@comunitea.com>
2020-03-26 10:33:00 +0100

    PEP8

--- project-addons/sale_margin_custom/models/res_partner.py
+++ project-addons/sale_margin_custom/models/res_partner.py
@@ -4,22 +4,45 @@
 from odoo import api, fields, models
 from odoo.addons import decimal_precision as dp
 
+
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    average_sale_margin = fields.Float(compute='_compute_average_sale_margin', string="Average margin (%)",
-                                   digits=dp.get_precision('Product Price'))
-    total_sale_margin = fields.Monetary(compute='_compute_average_sale_margin', string="Average margin (€)")
+    average_sale_margin = fields.Float(
+        compute="_compute_average_sale_margin",
+        string="Average margin (%)",
+        digits=dp.get_precision("Product Price"),
+    )
+    total_sale_margin = fields.Monetary(
+        compute="_compute_average_sale_margin", string="Average margin (€)"
+    )
 
     @api.multi
     def _compute_average_sale_margin(self):
-        partner_id = 'partner_id'
-        domain = [(partner_id, 'child_of', self.ids), ('state', 'in', ('done', 'sale'))]
-        vals = self.env['sale.order'].read_group(domain, [partner_id, 'margin', 'amount_untaxed'], [partner_id], orderby='id')
-        res = dict((item[partner_id][0], {'count': item['partner_id_count'], 'margin': item['margin'], 'amount': item['amount_untaxed']}) for item in
-             vals)
+        partner_id = "partner_id"
+        domain = [
+            (partner_id, "child_of", self.ids),
+            ("state", "in", ("done", "sale")),
+        ]
+        vals = self.env["sale.order"].read_group(
+            domain,
+            [partner_id, "margin", "amount_untaxed"],
+            [partner_id],
+            orderby="id",
+        )
+        res = dict(
+            (
+                item[partner_id][0],
+                {
+                    "count": item["partner_id_count"],
+                    "margin": item["margin"],
+                    "amount": item["amount_untaxed"],
+                },
+            )
+            for item in vals
+        )
         for partner in self:
             rp = res.get(partner.id)
             if rp:
-                partner.total_sale_margin = rp['margin'] / rp['count']
-                partner.average_sale_margin = rp['margin']/rp['amount'] * 100
+                partner.total_sale_margin = rp["margin"] / rp["count"]
+                partner.average_sale_margin = rp["margin"] / rp["amount"] * 100
--- project-addons/stock_custom/models/stock_location.py
+++ project-addons/stock_custom/models/stock_location.py
@@ -1,10 +1,9 @@
 # © 2018 Comunitea
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo.tools.float_utils import float_compare, float_round, float_is_zero
+from odoo import models, fields
 
-from odoo import api, models, fields, _
 
 class StockLocation(models.Model):
-    _inherit = 'stock.location'
+    _inherit = "stock.location"
 
-    oldname = fields.Char('Importado como')
+    oldname = fields.Char("Importado como")
