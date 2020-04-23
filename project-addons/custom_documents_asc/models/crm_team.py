e72de8fcd1d91a7cb2f323b2fe0056fec949c638
vicentecom <vicente@comunitea.com>
2020-04-23 15:06:59 +0200

    [ADD] custom_documents_asc: Modificación en las cabeceras de los documentos. Si el documento tiene crm_team muestra los datos de logo, nombre y tagline asociados a él

---
+++ project-addons/custom_documents_asc/models/crm_team.py
@@ -0,0 +1,13 @@
+# © 2020 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import fields, models
+from odoo.addons.crm.models import crm_stage
+
+
+class CrmTeam(models.Model):
+
+    _inherit = 'crm.team'
+
+    team_tag_line = fields.Char()
+    team_name = fields.Char(string='Company Name')
+    team_logo = fields.Binary(rstring="Company Logo")
