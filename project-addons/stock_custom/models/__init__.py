cc3355be5977d8f40c82b03fa16a64e06714aaaf
Kiko Sánchez <kiko@comunitea.com>
2020-05-10 19:37:01 +0200

    [IMP] stock_custom. Attastrar el campo ready_to_send

--- project-addons/stock_custom/models/__init__.py
+++ project-addons/stock_custom/models/__init__.py
@@ -3,3 +3,4 @@
 from . import stock_location
 from . import res_partner
 from . import stock_picking
+from . import sale_order
--- project-addons/stock_custom/models/stock_picking.py
+++ project-addons/stock_custom/models/stock_picking.py
@@ -1,9 +1,16 @@
 # © 2020 Comunitea
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    ready_to_send = fields.Boolean()
+    ready_to_send = fields.Boolean(string="PDA Ready")
+
+    @api.depends('ready_to_send', 'move_type', 'move_lines.state', 'move_lines.picking_id')
+    @api.one
+    def _compute_state(self):
+        super()._compute_state()
+        if self.state == 'assigned' and not self.ready_to_send and self.picking_type_id.group_code.app_integrated:
+            self.state = 'waiting'
