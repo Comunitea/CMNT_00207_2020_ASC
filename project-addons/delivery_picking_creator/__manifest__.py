866e0ecd98cd0425c1d8614e9cb441df83599e26
Kiko Sánchez <kiko@comunitea.com>
2020-05-10 19:48:09 +0200

    [FIX] delivery_picking_creator cambiar el nombre

--- project-addons/delivery_picking_creator/__manifest__.py
+++ project-addons/delivery_picking_creator/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Stock Move Serial List",
+    "name": "Stock Picking Delivery Creator",
     "summary": "Module to create delivery pickings for same partner",
     "version": "12.0.1.0.0",
     "author": "Comunitea",
