92850cfe0af895c6a69954c7dc0fa87c3ba35d75
vicentecom <vicente@comunitea.com>
2020-06-15 18:36:24 +0200

    [ADD] stock_picking_batch_delivery_carrier_cex: Modificaciones para añadir las funciones de Correos Express al batch picking

---
+++ project-addons/stock_picking_batch_delivery_carrier_cex/__manifest__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#    License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+#    Copyright (C) 2020 Comunitea Servicios Tecnológicos S.L. All Rights Reserved
+#    Vicente Ángel Gutiérrez <vicente@comunitea.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Stock Picking Batch Correos Express Connector',
+    'version': '12.0.0.0.0',
+    'summary': 'Correos Express Integration for Stock Picking Batches',
+    'category': 'Custom',
+    'author': 'comunitea',
+    'website': 'www.comunitea.com',
+    'license': 'AGPL-3',
+    'depends': [
+        'stock_picking_batch_delivery_carrier_base',
+        'delivery_carrier_label_cex'
+    ],
+    'data': [
+        'views/stock_picking_batch.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
