90d1677bcd984a2ff7e6159e2451c0cc9b49818f
JesusVMayor <jesus@comunitea.com>
2020-06-19 16:09:10 +0200

    fix

--- project-addons/stock_picking_batch_delivery_carrier_base/models/__init__.py
+++ project-addons/stock_picking_batch_delivery_carrier_base/models/__init__.py
@@ -17,7 +17,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from . import stock_picking_batc
+from . import stock_picking_batch
 from . import delivery_carrier
 from . import account_payment_mode
 from . import sale
