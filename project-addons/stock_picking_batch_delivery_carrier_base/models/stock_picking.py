bd378c5535ce8c512274b361e7c02c4a578d33fd
JesusVMayor <jesus@comunitea.com>
2020-06-22 18:55:49 +0200

    [FIX]Tipo de campo

--- project-addons/stock_picking_batch_delivery_carrier_base/models/stock_picking.py
+++ project-addons/stock_picking_batch_delivery_carrier_base/models/stock_picking.py
@@ -7,7 +7,7 @@ class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
     ready_to_send = fields.Boolean(string="PDA Ready", default=True)
-    pickup_signature = fields.Boolean()
+    pickup_signature = fields.Binary()
 
     @api.depends('ready_to_send', 'move_type', 'move_lines.state', 'move_lines.picking_id')
     @api.one
