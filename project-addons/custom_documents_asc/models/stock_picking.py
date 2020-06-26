b8d1fc30f64b852b80578ff9765ba0824bf96a6f
JesusVMayor <jesus@comunitea.com>
2020-06-26 22:18:08 +0200

    [BUILDOUT]Eliminado merge.

--- project-addons/custom_documents_asc/models/stock_picking.py
+++ project-addons/custom_documents_asc/models/stock_picking.py
@@ -1,10 +1,12 @@
 # © 2020 Comunitea
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import fields, models, api, _
+from odoo import fields, models
+
 
 class Picking(models.Model):
     _inherit = 'stock.picking'
 
+    mail_sended = fields.Boolean()
     team_id = fields.Many2one(related='sale_id.team_id')
     scheduled_date_report = fields.Date(compute='_compute_scheduled_date_report')
 
