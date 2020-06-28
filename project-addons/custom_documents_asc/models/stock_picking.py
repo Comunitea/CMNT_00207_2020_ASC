dea3eb347614db9efd3aa0d25c708fd307e50884
JesusVMayor <jesus@comunitea.com>
2020-06-28 11:24:07 +0200

    [FIX]custom_documents_asc: Corregido team_id de stock.picking.

--- project-addons/custom_documents_asc/models/stock_picking.py
+++ project-addons/custom_documents_asc/models/stock_picking.py
@@ -1,15 +1,34 @@
 # © 2020 Comunitea
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class Picking(models.Model):
     _inherit = 'stock.picking'
 
     mail_sended = fields.Boolean()
-    team_id = fields.Many2one(related='sale_id.team_id', store=True)
+    team_id = fields.Many2one()
     scheduled_date_report = fields.Date(compute='_compute_scheduled_date_report')
 
     def _compute_scheduled_date_report(self):
         for pick in self:
             pick.scheduled_date_report = pick.scheduled_date.date()
+
+    @api.model
+    def create(self, vals):
+        if 'team_id' not in vals:
+            partner = self.env['res.partner'].browse(vals.get('partner_id'))
+            if partner:
+                vals['team_id'] = partner.commercial_partner_id.team_id.id
+        return super().create(vals)
+
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    def _get_new_picking_values(self):
+        vals = super()._get_new_picking_values()
+        team_id = self.sale_line_id.order_id.team_id
+        if team_id:
+            vals['team_id'] = team_id.id
+        return vals
