86e090a859e39eee58c6a66179ff1dedacc01d92
JesusVMayor <jesus@comunitea.com>
2020-06-29 09:26:00 +0200

    [FIX]stock_custom: Sobreescribimos compute para evitar recursividad.

--- project-addons/stock_custom/models/stock_picking.py
+++ project-addons/stock_custom/models/stock_picking.py
@@ -11,13 +11,37 @@ class StockPicking(models.Model):
     @api.depends('ready_to_send', 'move_type', 'move_lines.state', 'move_lines.picking_id')
     @api.one
     def _compute_state(self):
-        super()._compute_state()
-        if self.state == 'assigned' \
-                and not self.ready_to_send \
-                and self.picking_type_id.code == 'outgoing' \
-                and self.picking_type_id.group_code.app_integrated:
-            self.state = 'waiting'
-
+        ''' State of a picking depends on the state of its related stock.move
+        - Draft: only used for "planned pickings"
+        - Waiting: if the picking is not ready to be sent so if
+          - (a) no quantity could be reserved at all or if
+          - (b) some quantities could be reserved and the shipping policy is "deliver all at once"
+        - Waiting another move: if the picking is waiting for another move
+        - Ready: if the picking is ready to be sent so if:
+          - (a) all quantities are reserved or if
+          - (b) some quantities could be reserved and the shipping policy is "as soon as possible"
+        - Done: if the picking is done.
+        - Cancelled: if the picking is cancelled
+        '''
+        if not self.move_lines:
+            self.state = 'draft'
+        elif any(move.state == 'draft' for move in self.move_lines):  # TDE FIXME: should be all ?
+            self.state = 'draft'
+        elif all(move.state == 'cancel' for move in self.move_lines):
+            self.state = 'cancel'
+        elif all(move.state in ['cancel', 'done'] for move in self.move_lines):
+            self.state = 'done'
+        else:
+            relevant_move_state = self.move_lines._get_relevant_state_among_moves()
+            if relevant_move_state == 'partially_available':
+                self.state = 'assigned'
+            else:
+                self.state = relevant_move_state
+            if (relevant_move_state == 'partially_available' or relevant_move_state == 'assigned') \
+                    and not self.ready_to_send \
+                    and self.picking_type_id.code == 'outgoing' \
+                    and self.picking_type_id.group_code.app_integrated:
+                self.state = 'waiting'
 
     @api.multi
     def _create_backorder(self, backorder_moves=None):
