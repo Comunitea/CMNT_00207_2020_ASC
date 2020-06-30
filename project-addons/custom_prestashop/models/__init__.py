a83ec7bda503275c9ccc799623f4198d373da35c
JesusVMayor <jesus@comunitea.com>
2020-07-01 00:08:48 +0200

    [IMP]custom_prestashop: Se evita duplicar transportistas

--- project-addons/custom_prestashop/models/__init__.py
+++ project-addons/custom_prestashop/models/__init__.py
@@ -16,3 +16,4 @@ from . import product_image
 from . import product_category
 from . import account_fiscal_position
 from . import stock_tracking
+from . import delivery_carrier
---
+++ project-addons/custom_prestashop/models/delivery_carrier/importer.py
@@ -0,0 +1,21 @@
+# © 2020 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo.addons.component.core import Component
+from odoo.addons.connector.components.mapper import mapping, only_create
+
+
+class DeliveryCarrierMapper(Component):
+    _inherit = "prestashop.delivery.carrier.import.mapper"
+
+    @mapping
+    def prestashop_unique_id(self, record):
+        return {'prestashop_unique_id': record['id']}
+
+    @only_create
+    @mapping
+    def odoo_id(self, record):
+        carrier_exists = self.env['delivery.carrier'].search(
+            [('prestashop_unique_id', '=', record['id'])])
+        if carrier_exists:
+            return{'odoo_id': carrier_exists.id}
+        return {}
