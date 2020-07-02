d9cf0c4fe83d04764991b0b371726c7b724bb045
Kiko Sánchez <kiko@comunitea.com>
2020-07-02 11:58:19 +0200

    [IMP] custom_documents_asc,stock_custom,warehouse_apk_custom

--- project-addons/custom_documents_asc/__manifest__.py
+++ project-addons/custom_documents_asc/__manifest__.py
@@ -22,7 +22,8 @@
         'stock',
         'stock_available_unreserved',
         'account_due_dates_str',
-        'stock_picking_batch_delivery_carrier_base'
+        'stock_picking_batch_delivery_carrier_base',
+        'stock_custom'
     ],
     'data': [
         'views/report_templates.xml',
