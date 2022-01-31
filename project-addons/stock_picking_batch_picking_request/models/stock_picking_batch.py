e10219d867cb42d4013683e0c8ffe49980fac948
vicentecom <vicente@comunitea.com>
2022-01-31 09:56:15 +0100

    [FIX] stock_picking_batch_picking_request: Peso mínimo 2.0 kg

--- project-addons/stock_picking_batch_picking_request/models/stock_picking_batch.py
+++ project-addons/stock_picking_batch_picking_request/models/stock_picking_batch.py
@@ -74,8 +74,8 @@ class StockBatchPicking(models.Model):
             )
             weight += (move_line.product_id.weight or 0.0) * qty
 
-        if weight <= 1.0:
-            return 1
+        if weight <= 2.0:
+            return 2
 
         return weight
 
