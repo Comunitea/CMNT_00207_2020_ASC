f7a438b749e4241d16e30823ac19197f00c4792e
Kiko <kiko@comunitea.com>
2022-03-29 13:53:40 +0200

    stock_cusotm. Variable replenish con stock 0

--- project-addons/stock_custom/models/product.py
+++ project-addons/stock_custom/models/product.py
@@ -73,26 +73,26 @@ class ProductProduct(models.Model):
         return
 
 
-    def get_lt_changes(self, lines):
+    def get_lt_changes(self, lines, offset_days = 0):
         self.ensure_one()
         res = False
         rt = self.replenish_type
         if not rt.use_lt:
             return res
-        date_ago = datetime.now() - relativedelta(days=rt.lt_days)
+        date_ago = datetime.now() - relativedelta(days=rt.lt_days + offset_days)
         lines = lines.filtered(lambda x: x.create_date >= date_ago)
         total_sales = len(lines.mapped("order_id"))
         if total_sales <= rt.lt_sales:
             res = rt.lt_qty
         return res
 
-    def get_gt_changes(self, lines):
+    def get_gt_changes(self, lines, offset_days = 0):
         self.ensure_one()
         res = False
         rt = self.replenish_type
         if not rt.use_gt:# or not (rt.gt_sales and rt.gt_days and rt.gt_qty):
             return res
-        date_ago = datetime.now() - relativedelta(days=rt.gt_days)
+        date_ago = datetime.now() - relativedelta(days=rt.gt_days + offset_days)
         lines = lines.filtered(lambda x: x.create_date >= date_ago)
         total_sales = len(lines.mapped("order_id"))
         if total_sales >= rt.gt_sales:
@@ -114,6 +114,82 @@ class ProductProduct(models.Model):
         return self.env["stock.move"].search(sale_domain, order=order).mapped('sale_line_id')
 
     def get_variable_replenish(self, max_days = 0 ):
+
+        ## MODIFICO PARA TENER EN CUENTA LOS DÍAS SIN STOCK
+        for product in self:
+            print ("Product  {}".format(product.display_name))
+            rt = product.replenish_type
+            if not rt:
+                continue
+
+            if not product.orderpoint_ids:
+                swo_vals = {'product_id': product.id, 'product_min_qty': 0, 'product_max_qty': 0}
+                self.env['stock.warehouse.orderpoint'].create(swo_vals)
+
+            sale_days = max(rt.gt_days, rt.lt_days,rt.sale_days, max_days)
+
+            ## Si hay días sin stock, entonces busco las ventas de los días anteriores que no tengan stock
+            if product.days_with_no_stock_count > 1:
+                days_with_no_stock_count = product.days_with_no_stock_count
+            else:
+                days_with_no_stock_count = 0
+            sale_days += days_with_no_stock_count
+            lines = product.get_sale_line_by_date(sale_days)
+            min_qty = rt.min_qty
+            max_qty = rt.max_qty
+
+            min_qty2 = 0
+            max_qty2 = 0
+
+            lt_change_qty = product.get_lt_changes(lines, days_with_no_stock_count)
+            if lt_change_qty:
+                min_qty = lt_change_qty
+                max_qty = lt_change_qty
+
+            if not lt_change_qty:
+                gt_change_qty = product.get_gt_changes(lines, days_with_no_stock_count)
+                if gt_change_qty:
+                    min_qty = gt_change_qty
+                    max_qty = gt_change_qty
+
+            # COMPUTE ALGORITM MIN MAX
+          
+            date_ago = datetime.now() - relativedelta(days=rt.sale_days + days_with_no_stock_count)
+            sale_lines = lines.filtered(lambda x: x.order_id.confirmation_date >= date_ago).sorted(lambda x:x.order_id.confirmation_date)
+          
+            total_sales = len(sale_lines.mapped("order_id"))
+            total_qty = sum(sale_lines.mapped(rt.qty_field))
+            average = 0
+
+            date_ago = datetime.now() - relativedelta(days=30 + days_with_no_stock_count)
+            last_month_lines = lines.filtered(lambda x: x.order_id.confirmation_date >= date_ago)
+            total_month_sales = len(
+                last_month_lines.mapped("order_id")
+            )
+
+            # MENOS DE DOS VENTAS ÚLTIMOS 30 DÍAS IMPLICA COGER EL MIN/MAX
+            # POR DEFECTO, QUE PUEDE HABE CAMBIADO
+            if total_sales and total_month_sales > 2:
+                average = (total_qty / total_sales) * rt.average_ratio
+                # Get qty by order where qty under average
+                for sale_line in sale_lines.filtered(lambda x: x[rt.qty_field] < average):
+                    max_qty2 += sale_line[rt.qty_field]
+            min_qty2 = max_qty2 * rt.min_qty_ratio
+
+            # UPDATE REPLENISH RULES
+            if max_qty and min_qty:
+                vals = {
+                    "product_min_qty": max(min_qty, min_qty2),
+                    "product_max_qty": max(max_qty, max_qty2),
+                }
+                if product.orderpoint_ids:
+                    product.orderpoint_ids.write(vals)
+                else:
+                    swo = self.env["stock.warehouse.orderpoint"]
+                    vals.update(product_id=product.id)
+                    swo.create(vals)
+
+    def get_variable_replenish_bis(self, max_days = 0 ):
         for product in self:
             rt = product.replenish_type
             if not rt:
